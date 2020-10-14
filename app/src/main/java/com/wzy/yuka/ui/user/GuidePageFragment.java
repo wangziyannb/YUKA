dd11d88e338e0e33680dff96bbebe9611eb1cc71
tajinxiongnb <17685807928@163.com>
2020-10-14 17:11:30 +0800

    引导菜单页面
    Signed-off-by: Thomsen <17685807928@163.com>

--- app/src/main/java/com/wzy/yuka/ui/user/GuidePageFragment.java
+++ app/src/main/java/com/wzy/yuka/ui/user/GuidePageFragment.java
@@ -1,5 +1,7 @@
 package com.wzy.yuka.ui.user;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,11 +16,28 @@ import com.wzy.yuka.R;
 /**
  * author:tajinxiong
  */
-public class GuidePageFragment extends Fragment {
+public class GuidePageFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.main_guide, container, false);
+        root.findViewById(R.id.Narcissu1).setOnClickListener(this);
+        root.findViewById(R.id.Narcissu2).setOnClickListener(this);
         return root;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        switch (v.getId()) {
+            case R.id.Narcissu1:
+                intent.setData(Uri.parse("https://www.bilibili.com/video/BV1hK411T7gq"));
+                startActivity(intent);
+            case R.id.Narcissu2:
+                intent.setData(Uri.parse("https://www.bilibili.com/video/BV1yi4y1u7uM"));
+                startActivity(intent);
+        }
     }
 }
